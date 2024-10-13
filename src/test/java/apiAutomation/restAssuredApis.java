57bc1006245cc25569b325368f4c16988f67bbca
Saima Hanif <45038555+saimahanif38@users.noreply.github.com>
2024-10-13 18:17:47 +0500

    added more apis method

--- src/test/java/apiAutomation/restAssuredApis.java
+++ src/test/java/apiAutomation/restAssuredApis.java
@@ -3,9 +3,13 @@ package apiAutomation;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import static org.hamcrest.Matchers.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class restAssuredApis {
 
@@ -15,52 +19,73 @@ public class restAssuredApis {
     @BeforeClass
     public void setup() {
         // Set base URI for all tests
-        RestAssured.baseURI = "https://app.respond.io/";
+        baseURI = "https://app.respond.io/";
     }
 
     @Test
-    public void testLogin() {
+    public void testLoginPostRequest() {
         // Create request request body for login
         String requestBody = "{\"email\":\"saimahanif1938+00@gmail.com\",\"password\":\"Click123#\"}";
 
         // Send Get request to the login endpoint
-        Response loginResponse = RestAssured.given()
+        Response loginResponse = given()
                 .contentType(ContentType.JSON)
                 .body(requestBody)
-                .post("auth/login");
-       
+            .when()
+                .post("auth/login")
+            .then()
+                .assertThat()
+                .statusCode(200)
+                .body("message", equalTo("Login successful"))  // Asserting the message in the response
+                .body("status", equalTo("success")) // Asserting the status in the response
+                .body("data.idToken", notNullValue()) // Asserting the presence of token in the response
+                .extract()
+                .response();
 
         // Validate login response status code
         Assert.assertEquals(loginResponse.getStatusCode(), 200, "Expected status code is 200");
+        
+        // Save token for accessing other pages after login
         token = loginResponse.jsonPath().getString("data.idToken");
     }
 
-    @Test(dependsOnMethods = "testLogin")
-    public void testSecurityPage() {
+    @Test(dependsOnMethods = "testLoginPostRequest")
+    public void testSecuritySSOGetRequest() {
         // Use the token retrieved in the testLogin method
-        Response apiResponse = RestAssured.given()
-                .header("Authorization", "Bearer " + token)
-                .header("orgid", "249553")
-                .get("api/organization/249553/security");
-
-        // Validate the response
-        Assert.assertEquals(apiResponse.getStatusCode(), 200, "Expected status code is 200");
+    	given()
+	    	.header("Authorization", "Bearer " + token)
+	        .header("orgid", "249553")
+        .when()
+        	.get("api/organization/249553/security/sso")
+        .then()
+	        .assertThat()
+	        .statusCode(403)
+	        .body("message", equalTo("Access denied: Your organization subscription trial has ended. Please subscribe to a new plan."));
     }
     
-    @Test(dependsOnMethods = "testLogin")
-    public void testUpdateProfile() {
-        String requestBody = "{\"firstName\":\"ssss\",\"lastName\":\"User\",\"lang\":\"en\"}";
+    @Test(dependsOnMethods = "testLoginPostRequest")
+    public void testUpdateProfilePutRequest() {
+    	 // Generate the current timestamp
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 
+        // Create a dynamic firstName using the timestamp
+        String dynamicFirstName = "Saima Rehman" + timeStamp;
+        
+        // Create request body
+        String requestBody = "{\"firstName\":\""+ dynamicFirstName +"\",\"lastName\":\"User\",\"lang\":\"en\"}";
     	
         // Use the token retrieved in the testLogin method
-        Response apiResponse = RestAssured.given()
-                .header("Authorization", "Bearer " + token)
-                .header("orgid", "249553")
-                .contentType(ContentType.JSON)
-                .body(requestBody)
-                .put("auth/user/profile");
-
-        // Validate the response
-        Assert.assertEquals(apiResponse.getStatusCode(), 200, "Expected status code is 200");
+        given()
+	        .header("Authorization", "Bearer " + token)
+	        .header("orgid", "249553")
+	        .contentType(ContentType.JSON)
+	        .body(requestBody)  // Sending the request body
+        .when()
+        	.put("auth/user/profile")  // Sending a PUT request to update the user profile
+        	.then()
+        	.assertThat()
+	        .statusCode(200)  // Validate the response status code is 200 (OK)
+	        .body("message", equalTo("Profile updated successfully"))  // Match updated body: message
+	        .body("status", equalTo("success"));  // Match updated body: status
     }
 }
